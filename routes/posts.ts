ee83a507990177827aeda8ba86ded823e55a5656
Satyxa <satyxa1919@gmail.com>
2023-08-26 19:04:00 +0300

    add mongo url

--- routes/posts.ts
+++ routes/posts.ts
@@ -66,11 +66,11 @@ export const postCreateValidation = async (req: Request, res: Response, next: Ne
 export const postsRouter = Router({})
 
 postsRouter.get('/', async (req: Request, res: Response) => {
-        const {page} = req.query || 1
+        const {page} = req.query.page ? req.query.page : 1
         const posts = await patreonPosts.find({}, { projection : { _id:0 }}).skip(10 * page - 10).limit(10).toArray()
         const totalCount = await patreonPosts.count({})
         const pagesCount = Math.ceil(totalCount / 10)
-        return res.status(200).send({pagesCount,page,pageSize:10,totalCount,items: posts})
+        return res.status(200).send({pagesCount,page: req.query.page ? req.query.page : 1,pageSize:10,totalCount,items: posts})
 })
 // @ts-ignore
 postsRouter.get('/:id', async (req: Request, res: Response) => {
