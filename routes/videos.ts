0efdf7b83eb2f824e1703f644769f28dfcc465df
Satyxa <satyxa1919@gmail.com>
2023-08-26 14:06:31 +0300

    add mongo url

--- routes/videos.ts
+++ routes/videos.ts
@@ -38,9 +38,11 @@ const createVideoValidation = (title: string, author: string,
   if(!availableResolutions) {
     errors.push({message: 'invalid availableResolutions', field: 'availableResolutions'})
   } else if(availableResolutions){
-    if(availableResolutions[1] === "Invalid"){
-      errors.push({message: 'invalid avail222ableResolutions', field: 'availableResolutions'})
-    }
+        availableResolutions.map(ar => {
+            if(!AvRes.includes(ar)){
+                errors.push({message: 'invalid avail222ableResolutions', field: 'availableResolutions'})
+            }
+        })
   }
   return errors
 }
