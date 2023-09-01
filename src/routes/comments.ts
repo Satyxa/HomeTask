141cab173320a15195c2f4ee5035bf009bba135a
Satyxa <satyxa1919@gmail.com>
2023-09-01 21:08:25 +0300

    2050

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -4,6 +4,7 @@ import {postT} from '../types'
 import * as uuid from 'uuid'
 import {checkAuth, postCreateValidation} from "../validation";
 import {paginationSort} from "../PaginationAndSort";
+import {AuthMiddleware} from "../AuthMiddleware";
 
 
 export const commentsRouter = Router({})
@@ -19,8 +20,10 @@ commentsRouter.get('/:id', async (req: Request, res: Response) => {
     return res.status(200).send(comment)
 })
 
-commentsRouter.put('/:id', async(req:Request, res:Response) => {
+commentsRouter.put('/:id', AuthMiddleware, async(req:Request, res:Response) => {
     const id = req.params.id
+    const comment = await patreonComments.findOne({id})
+    if(comment.userId !== req.userId) return res.sendStatus(403)
     const content = req.body.content
     const result = await patreonComments
         .updateOne({id},
@@ -32,8 +35,10 @@ commentsRouter.put('/:id', async(req:Request, res:Response) => {
     }
 })
 
-commentsRouter.delete('/:id', async (req: Request, res: Response) => {
+commentsRouter.delete('/:id', AuthMiddleware, async (req: Request, res: Response) => {
     const id = req.params.id
+    const comment = await patreonComments.findOne({id})
+    if(comment.userId !== req.userId) return res.sendStatus(403)
     const result = await patreonComments.deleteOne({id})
     if(result.deletedCount === 0){
         return res.sendStatus(404)
