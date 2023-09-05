a58dcc2002464b64576a1975edfaf0b40e17babf
Satyxa <satyxa1919@gmail.com>
2023-09-05 11:50:38 +0300

    2050

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -3,12 +3,14 @@ import {Router, Request, Response} from "express";
 import {emailAdapter} from "../email-adapter";
 import {patreonUsers} from "../db/db";
 import {registrationRouter} from "./registration";
-
+import * as uuid from 'uuid'
+import {UserAccountDBType} from "../types";
+import {ModifyResult, UpdateResult} from "mongodb";
 export const emailRouter = Router({})
 
 emailRouter.post('/registration-confirmation', async (req: Request, res: Response) => {
     try {
-        const code = req.query.code
+        const code = req.query.code || req.body.code
         if(!code) return res.sendStatus(400)
         const result = await patreonUsers.updateOne({"EmailConfirmation.confirmationCode": code}, {$set: {
                 "EmailConfirmation.isConfirmed": true
@@ -32,7 +34,17 @@ registrationRouter.post('/registration-email-resending', async (req: Request, re
     const email: string = req.body.email
     if (!email) return res.sendStatus(400)
     const user = await patreonUsers.findOne({'AccountData.email': email})
+    const userWithUpdatedCode: ModifyResult<UserAccountDBType> = await patreonUsers.findOneAndUpdate(
+        {'AccountData.email': email},
+        {$set: {'EmailConfirmation.confirmationCode': uuid.v4()}},
+    {returnDocument: 'after'}
+    )
+    const newCode = userWithUpdatedCode.value!.EmailConfirmation.confirmationCode
     if(!user) return res.sendStatus(404)
     else if(user.EmailConfirmation.isConfirmed) return res.sendStatus(400)
-    await emailAdapter.sendEmail(email, 'email confirmation', user.EmailConfirmation.confirmationCode, res)
+    const message = `<h1>Thank for your registration</h1>
+    <p>To finish registration please follow the link below:
+        <a href='https://home-task-f6h9.vercel.app/auth/registration-confirmation?code=${newCode}'>complete registration</a>
+    </p>`
+    await emailAdapter.sendEmail(email, 'email confirmation', message, res)
 })
--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -11,9 +11,6 @@ export const registrationRouter = Router({})
 registrationRouter.post('/registration', ...registerValidation, checkValidation,  async(req: Request, res: Response) => {
     const {email, login, password} = req.body
     if(!email || !login || !password) return res.sendStatus(401)
-    const filter: Filter<userT> = {$or: [{'AccountData.username': {login, $options: 'i'}}, {'AccountData.email': {email, $options: 'i'}}]}
-    const checkLoginAndEmail = await patreonUsers.findOne(filter, { projection : { _id:0, passwordHash: 0, passwordSalt: 0 }})
-    if (checkLoginAndEmail) return res.sendStatus(400)
     const newUser: UserAccountDBType = await createUser(login, email, password)
     await patreonUsers.insertOne({...newUser})
     console.log('before email send')
