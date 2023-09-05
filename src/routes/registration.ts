5755c50b0e712d2f801aac4a764e9319598c2d56
Satyxa <satyxa1919@gmail.com>
2023-09-05 11:17:32 +0300

    2050

--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -19,7 +19,7 @@ registrationRouter.post('/registration', ...usersValidation, checkValidation,  a
     console.log('before email send')
     await emailAdapter.sendEmail(newUser.AccountData.email, 'Confirm your email', `<h1>Thank for your registration</h1>
     <p>To finish registration please follow the link below:
-        <a href='https://home-task-f6h9.vercel.app/registration-confirmation?code=${newUser.EmailConfirmation.confirmationCode}'>complete registration</a>
+        <a href='https://home-task-f6h9.vercel.app/auth/registration-confirmation?code=${newUser.EmailConfirmation.confirmationCode}'>complete registration</a>
     </p>`, res)
 
     return res.status(204)
